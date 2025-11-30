1b35d3368842d7db85fe0539bee7b9c6493a43d7
Arun N Kumar <gettoarun@gmail.com>
2025-11-29 19:25:56 -0500

    Basic sprites loaded for intro screen. Pushing to edit it raw.

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
- 
+function createSprites () {
+    BrownWall = sprites.create(assets.image`Brown Wall`, 0)
+    Fire = sprites.create(assets.image`Fire`, 0)
+    Crown = sprites.create(assets.image`Crown`, 0)
+    BrownWallMini = sprites.create(assets.image`Brown Wall Mini`, 0)
+}
+let BrownWallMini: Sprite = null
+let Crown: Sprite = null
+let Fire: Sprite = null
+let BrownWall: Sprite = null
+scene.setBackgroundColor(15)
+let Dave = sprites.create(assets.image`Dave Walk`, SpriteKind.Player)
+controller.moveSprite(Dave, 100, 100)
+tiles.setCurrentTilemap(tilemap`intro`)
